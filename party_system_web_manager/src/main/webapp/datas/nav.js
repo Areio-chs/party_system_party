ac285126f72bdb142aa31f124b795ede18051538
Areio <2542621856@qq.com>
2020-10-22 18:57:10 +0800

    提交前端代码

---
+++ party_system_web_manager/src/main/webapp/datas/nav.js
@@ -0,0 +1,140 @@
+var navs = [{
+	"title": "组织管理",
+	"icon": "fa-cubes",
+	"spread": true,
+	"children": [{
+		"title": "党总支",
+		"icon": "&#xe641;",
+		"href": "general.html"
+	}, {
+		"title": "党支部",
+		"icon": "&#xe63c;",
+		"href": "party.html"
+	}, {
+		"title": "党小组",
+		"icon": "&#xe63c;",
+		"href": "group.html"
+	},{
+		"title": "团支部",
+		"icon": "&#xe63c;",
+		"href": "youthLeagueBranch.html"
+	}, {
+		"title": "党组织关系树",
+		"icon": "&#xe609;",
+		"href": "partyTree.html"
+	}]
+}, {
+	"title": "成员管理",
+	"icon": "fa-cogs",
+	"spread": false,
+	"children": [{
+		"title": "团员",
+		"icon": "fa-address-book",
+		"href": "leagueMember.html"
+	},{
+		"title": "入党申请人",
+		"icon": "&#xe609;",
+		"href": "applicant.html"
+	},{
+		"title": "积极分子",
+		"icon": "fa-table",
+		"href": "activists.html"
+	}, {
+		"title": "发展对象",
+		"icon": "fa-navicon",
+		"href": "develop.html"
+	}, {
+		"title": "预备党员",
+		"icon": "&#xe62a;",
+		"href": "pre_member.html"
+	}, {
+		"title": "正式党员",
+		"icon": "&#xe628;",
+		"href": "member.html"
+	}]
+}, {
+	"title": "党员发展",
+	"icon": "&#x1002;",
+	"spread": false,
+	"children": [{
+		"title": "确定入党积极分子",
+		"icon": "fa-check-square-o",
+		"href": "checkActivist.html"
+	},{
+		"title": "确定发展对象",
+		"icon": "fa-check-square-o",
+		"href": "checkDevelop.html"
+	},{
+		"title": "接收预备党员",
+		"icon": "fa-check-square-o",
+		"href": "checkPreMember.html"
+	},{
+		"title": "预备党员转正",
+		"icon": "fa-check-square-o",
+		"href": "checkMember.html"
+	}]
+}, {
+	"title": "党组织关系转接",
+	"icon": "fa-address-book",
+	"href": "",
+	"spread": false,
+	"children": [{
+		"title": "关系转入待审核",
+		"icon": "fa-github",
+		"href": "changeFrom.html"
+	}, {
+		"title": "关系转入已审核",
+		"icon": "fa-qq",
+		"href": "changedFrom.html"
+	}, {
+		"title": "关系转出待审核",
+		"icon": "&#xe609;",
+		"href": "changeTo.html"
+	}, {
+		"title": "关系转出已审核",
+		"icon": "fa-weibo",
+		"href": "changedTo.html"
+	}]
+}, {
+	"title": "集体活动",
+	"icon": "fa-stop-circle",
+	"href": "activity.html",
+	"spread": false
+},{
+	"title": "培养教育",
+	"icon": "fa-stop-circle",
+	"href": "study.html",
+	"spread": false
+},{
+	"title": "权限管理",
+	"icon": "fa-stop-circle",
+	"href": "#",
+	"spread": false,
+	"children": [{
+		"title": "用户列表",
+		"icon": "fa-github",
+		"href": "user.html"
+	},{
+		"title": "角色列表",
+		"icon": "fa-github",
+		"href": "roles.html"
+	},{
+		"title": "权限列表",
+		"icon": "fa-github",
+		"href": "power.html"
+	}]
+},{
+	"title": "日志管理",
+	"icon": "fa-stop-circle",
+	"href": "#",
+	"spread": false,
+	"children": [{
+		"title": "操作日志",
+		"icon": "fa-github",
+		"href": "log.html"
+	},{
+		"title": "登录日志",
+		"icon": "fa-github",
+		"href": "loginlog.html"
+	}]
+}];
